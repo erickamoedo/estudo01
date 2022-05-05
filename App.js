4d3dbc151d0936b6618562e1a45ae7fa61b1022c
Erick Amoedo <erick.amoedo@live.com>
2022-05-04 23:28:35 -0300

    Finish

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { Text, View, Button, Style} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ 
+
+export default class mainScreen extends Component {
+ 
+  handleClick = () => {
+
+    alert("test")
+    console.log("Let'ss get down.")
+
+  }
+
+
+  render() {
+      return (
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  backgroundColor: "#000",        
+                }}>
+                <Text
+                style={{color: "#fff"}}    
+                >Oi pessoal</Text>
+
+                  <View  style={{paddingTop:100}}>
+                    <button
+                    onClick={() => console.log('Click')}>Botão HTML</button>
+                  </View>
+
+                  <View  style={{paddingTop:100}}>
+                    <Button                      
+                   
+                      onPress={this.handleClick}
+                      title="Botão React"
+                    />
+                  </View>                
+              </View>
+
+      );
+}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ 
+ 
+ 
